8f3ee4587d8521805ee856d3fbc0e024cf073204
cmd-source <conorcrosbie09@gmail.com>
2022-02-22 11:43:22 +0000

    changing wordle layout

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+function createSquares() {
+    const gameBoard = document.getElementById("board");
+
+    for (let index = 0; index < 30; index++) {
+      let square = document.createElement("div");
+      square.classList.add("square");
+      square.classList.add("animate__animated");
+      square.setAttribute("id", index + 1);
+      gameBoard.appendChild(square);
+    }
+  }
+});
